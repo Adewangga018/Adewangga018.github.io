f3f7b6fbb61c749a8238d10da014ba0e0300fe8f
Adewangga018 <arjuna.dewangga018@gmail.com>
2025-08-14 14:33:29 +0700

    Configure for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Atur base ke root karena nama repo Anda adalah <username>.github.io
+  base: '/', 
+
   plugins: [react()],
   resolve: {
     alias: {
